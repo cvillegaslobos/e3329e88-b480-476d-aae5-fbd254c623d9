a41300d9aff4ba07db8caecccaede267d87c6b63
Cesar Villegas <cvillegaslobos@gmail.com>
2017-10-23 20:47:08 +0000

    Breadcrumb de repo reubicada

--- resources/views/layouts/repo.blade.php
+++ resources/views/layouts/repo.blade.php
@@ -1,23 +1,6 @@
 @extends('layouts.master')
 
-@section('top')
-<div class="level-right">
-    
-<nav class="breadcrumb" aria-label="breadcrumbs">
-  <ul>
-    <li>
-        <span class="icon">
-            <i class="fa fa-home"></i>
-        </span>
-        <a href='{{ url("/repo/overview") }}'>Repositorios</a>
-    </li>
-    <li class="is-active"><a href="#" aria-current="page">{{ $response['name'] }}</a></li>
-    <li class="is-active"><a href="#" aria-current="page">@yield('titulo')</a></li>
-    
-  </ul>
-</nav>
-</div>
-@endsection
+
 
 @section('content')
 <div class="columns">
@@ -44,6 +27,20 @@
         </aside>
     </div>
     <div class="column is-three-quarters">
+        
+        <nav class="breadcrumb" aria-label="breadcrumbs">
+            <ul>
+                <li>
+                    <span class="icon">
+                        <i class="fa fa-home"></i>
+                    </span>
+                    <a href='{{ url("/repo/overview") }}'>Repositorios</a>
+                </li>
+                <li class="is-active"><a href="#" aria-current="page">{{ $response['name'] }}</a></li>
+                <li class="is-active"><a href="#" aria-current="page">@yield('titulo')</a></li>
+            </ul>
+        </nav>
+
         <h1 class="title is-4"> @yield('titulo') </h1>
         <h2 class="subtitle is-6"> @yield('subtitulo') </h2>
         
